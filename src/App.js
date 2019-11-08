ecc814fe325c92c272c9d9a654150cd0bce0d31e
deanovision <rodeanfraser@gmail.com>
2019-11-07 19:24:18 -0500

    updated routes to reflect new changes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import NavBar from "./components/NavBar";
 // New - import the React Router components, and the Profile page component
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
+import OnboardUser from "./components/OnboardUser"
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       {/* New - use BrowserRouter to provide access to /profile */}
       <BrowserRouter>
         <header>
-          <NavBar />
+         <Route path="/" component={NavBar} />
         </header>
         <Switch>
           <Route path="/" exact />
           <Route path="/profile" component={Profile} />
+          <Route path="/onboarduser" component={OnboardUser} />
         </Switch>
       </BrowserRouter>
     </div>
