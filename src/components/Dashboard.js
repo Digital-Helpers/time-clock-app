913f17f45223930551374b9900b991365d6ae360
deanovision <rodeanfraser@gmail.com>
2019-11-11 16:29:35 -0500

    created dashboard compnent

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react'
+import axios from 'axios';
+
+export default function Dashboard() {
+    useEffect(()=>{
+        //grab user info from state and send request to server to determine which user dashboard to display
+    })
+    return (
+        <div>
+            <h1>Dashboard</h1>            
+        </div>
+    )
+}
