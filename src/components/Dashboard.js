42fa21bfab92c7473a0ed5b76a3d458662e48499
deanovision <rodeanfraser@gmail.com>
2019-11-13 21:31:21 -0500

    added state to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import axios from 'axios';
 
 export default function Dashboard() {
+    const [userData, setUserData] = React.useState({})
     useEffect(()=>{
         //grab user info from state and send request to server to determine which user dashboard to display
     })
