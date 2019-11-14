048fb09cbf042984afd1d22be723e363d3798adf
deanovision <rodeanfraser@gmail.com>
2019-11-13 21:35:26 -0500

    updated useEffect

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,14 @@ import React, {useEffect} from 'react'
 import axios from 'axios';
 
 export default function Dashboard() {
-    const [userData, setUserData] = React.useState({})
+    const [userData, setUserData] = React.useState("")
     useEffect(()=>{
         //grab user info from state and send request to server to determine which user dashboard to display
+        axios.get('/')
+        .then(user => {
+            setUserData(user)
+        })
+        .catch(err=> console.log(err))
     })
     return (
         <div>
